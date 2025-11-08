84eabaa2d39001643f2d0a389c46eec546bb961f
Devell Robinson <devell.robinson@bordevtech.com>
2025-11-08 21:42:38 +0000

    feat: add middleware for authentication and update environment variables in configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    GITHUB_ID: "Ov23liVf6t7JVQAs2guJ",
+    GITHUB_SECRET: "b9ce2d03b211158b2ff8a2542508cba09b9f88ab",
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 export default nextConfig;
